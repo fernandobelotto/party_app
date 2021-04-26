f109850b2984cec11c032ef9440535ba2adc50f5
Fernando Belotto <fernando.bbosco@gmail.com>
2021-04-26 08:47:13 -0300

    Initial Commit

---
+++ party_app/party_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  party_appApp.swift
+//  party_app
+//
+//  Created by Fernando Gabriel Belotto Bosco on 26/04/21.
+//
+
+import SwiftUI
+
+@main
+struct party_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
