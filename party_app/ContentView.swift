40a26c98a93846b30d481f00794cc4a437a1b1d5
Fernando Belotto <fernando.bbosco@gmail.com>
2021-05-03 10:07:28 -0300

    tab bar added

--- party_app/ContentView.swift
+++ party_app/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        UITabBar.appearance().isTranslucent = false
+        UITabBar.appearance().barTintColor = UIColor(Color("black"))
+      }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Início", systemImage: "house.fill")
+                        .accentColor(.red)
+                }
+            TicketView()
+                .tabItem {
+                    Label("Tickets", systemImage: "ticket.fill")
+                }
+            ProfileView()
+                .tabItem {
+                    Label("Order", systemImage: "person.crop.circle.fill")
+                }
+        }.accentColor(Color("accent"))
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 12")
+        }
+    }
+}
+
+struct TicketView: View {
+    var body: some View {
+        Text("Tickets")
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Perfil")
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
     }
 }
