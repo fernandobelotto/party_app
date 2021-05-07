24b6b1b2f1ba1bcc58c6b070f58805ae0f2ef715
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2021-05-07 17:01:40 -0300

    Add Search, EventDetails, TicketChoice and TicketDetail views unfinished

--- party_app/Views/TicketChoiceView.swift
+++ party_app/Views/TicketChoiceView.swift
@@ -7,4 +7,83 @@
 
 import SwiftUI
 
+struct TicketChoiceView: View {
+    
+    @State var selected = [false, false, false]
+    @State var ticketCards = [
+        TicketCard(label: "PISTA", price: "R$20,00", color: Color("8")),
+        TicketCard(label: "VIP", price: "R$30,00", color: Color("8")),
+        TicketCard(label: "CAMAROTE", price: "R$40,00", color: Color("8"))
+    ]
+    
+    var body: some View {
+        ZStack (alignment: .center) {
+            Card(color: Color("Black"))
+            
+            VStack (alignment: .center) {
+                Text("Chopada da Agrícola")
+                    .modifier(MyText(color: Color("White"), type: .H2))
+                    .padding(.top, 24.0)
+                
+                Text("1º lote")
+                    .modifier(MyText(color: Color("White"), type: .Small))
+                    .padding(.top, 8.0)
+                
+                Spacer()
+                
+                VStack (alignment: .center) {
+                    ForEach(0 ..< 3) { i in
+                        Button(action: {
+                            self.select(i)
+                        }) {
+                            self.ticketCards[i]
+                        }
+                    }
+                }
+                .padding(.horizontal, 32.0)
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    MyButton(text: "Buy", type: .Primary)
+                }
+                .disabled(self.selected.allSatisfy({ isSelected in return !isSelected }))
+                .padding(.bottom, 24.0)
+            }
+        }
+        .padding(24.0)
+        .modifier(Background())
+    }
+    
+    func select(_ which: Int) {
+        for i in 0 ..< self.selected.count {
+            if i == which {
+                self.selected[i] = true
+                self.ticketCards[i].setColor(Color("1"))
+            } else {
+                self.selected[i] = false
+                self.ticketCards[i].setColor(Color("8"))
+            }
+        }
+    }
+}
 
+struct TicketCardButton: View {
+    let card: TicketCard
+    let action: () -> ()
+    
+    init(label: String, price: String, action: @escaping () -> ()) {
+        self.card = TicketCard(label: label, price: price, color: Color("8"))
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(action: {
+            self.action()
+        }) {
+            self.card
+        }
+    }
+}
--- party_app/Views/TicketDetailView.swift
+++ party_app/Views/TicketDetailView.swift
@@ -5,4 +5,66 @@
 //  Created by Leonardo de Sousa Rodrigues on 07/05/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct TicketDetailView: View {
+    var body: some View {
+        ZStack (alignment: .center) {
+            Card(color: Color("Black"))
+            
+            VStack (alignment: .center) {
+                Text("Chopada da Agrícola")
+                    .modifier(MyText(color: Color("White"), type: .H2))
+                    .padding(.top, 24.0)
+                
+                Spacer()
+                
+                VStack (alignment: .leading) {
+                    FieldValue(
+                        field: "name",
+                        value: "Juliana Moreira"
+                    )
+                    
+                    FieldValue(
+                        field: "local",
+                        value: "Barão Geraldo"
+                    )
+                    
+                    FieldValue(
+                        field: "date",
+                        value: "seg, 05/02/2047"
+                    )
+                    
+                    FieldValue(
+                        field: "time",
+                        value: "22:00h - 04:00h"
+                    )
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 32.0)
+                
+                Spacer()
+                
+                QRCode()
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    MyButton(text: "Save ticket", type: .Primary)
+                }
+                .padding(.bottom, 24.0)
+            }
+        }
+        .padding(24.0)
+        .modifier(Background())
+    }
+}
+
+struct QRCode: View {
+    var body: some View {
+        Card(color: Color("White"))
+            .frame(width: 120.0, height: 120.0)
+    }
+}
